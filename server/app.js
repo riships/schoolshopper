1acbdd137782f3836ba262016ab866482b061b27
Rishi Prakash Soni <116996040+riships@users.noreply.github.com>
2025-02-06 21:49:04 +0530

    feat: enhance user routes with authorization middleware and configure CORS for specific origin

--- server/app.js
+++ server/app.js
@@ -15,7 +15,14 @@ connectDb();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+// app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use('/public/uploads/', express.static(path.join(__dirname, '/public/uploads/')));
 
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,15 +1,16 @@
 import { createUser, deleteUser, forgetPassword, getUser, getUsers, loginUser, logoutUser, resetPassword, updateUser, verifyUserWithOtp } from "../controllers/user.controller.js";
 import { Router } from "express";
+import { authorize, protect } from "../middlewares/auth.middleware.js";
 const router = Router();
 
-router.post("/users", createUser);
+router.post("/users", protect, authorize('admin'), createUser);
 router.post("/users/verify", verifyUserWithOtp);
-router.get("/users", getUsers);
-router.get("/users/:id", getUser);
-router.put('/users/:id', updateUser);
-router.delete('/users/:id', deleteUser);
+router.get("/users", protect, authorize('admin'), getUsers);
+router.get("/users/:id", protect, getUser);
+router.put('/users/:id', protect, updateUser);
+router.delete('/users/:id', protect, deleteUser);
 router.post('/users/login', loginUser);
-router.get('/users/logout', logoutUser);
+router.get('/users/logout', protect, logoutUser);
 router.post('/users/forgetpassword', forgetPassword);
 router.put('/users/resetpassword/:resetToken', resetPassword);
 
