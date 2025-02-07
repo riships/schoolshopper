160a13cd8dfa844bc9cc56eed7a1f82fe2633006
Rishi <rishi.edunext@gmail.com>
2025-02-07 13:58:25 +0530

    feat: add react-toastify for user feedback and update login response message

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,7 +5,8 @@ import loginLeftImg from '../assets/images/login.png';
 import logo from '../assets/images/logo.png';
 import { Row, Col, Button } from 'react-bootstrap'
 const url = import.meta.env.VITE_API_URL;
-// demo
+import { ToastContainer, toast } from 'react-toastify';
+
 
 function Login() {
     const [email, setEmail] = useState("");
@@ -14,41 +15,45 @@ function Login() {
     const handleSubmit = async (e) => {
         try {
             e.preventDefault();
-
             const userCredentials = {
                 email,
                 password,
             }
+            console.log(email, password);
+
             const response = await axios.post(`${url}/api/users/login`, userCredentials,
                 {
                     withCredentials: true,
                     headers: {
                         'Content-Type': 'application/json',
-                        'x-auth-token': `${Cookies.get('token')}`
+                        'x-auth-token': `${Cookies.get('token')}` || ''
                     }
                 }
             );
             Cookies.set("token", response.data.token, { expires: 60 * 60 * 1000 });
+            console.log(response.data);
+            toast.success(response.data.message);
         } catch (error) {
-            console.error(error);
+            toast.error(error.response.data.message)
         }
     }
 
     return (
         <Row className=''>
+            <ToastContainer />
             <Col md={8}>
-                <img className='img-fluid min-vh-100' src={loginLeftImg} />
+                <img className='min-vh-100 img-fluid' src={loginLeftImg} />
             </Col>
             <Col md={4}>
-                <div className='text-center d-flex align-item-center justify-content-center flex-column h-100 px-5'>
-                    <img src={logo} className="mb-4 mx-auto w-50 img-fluid" alt="logo icon" />
+                <div className='d-flex flex-column justify-content-center align-item-center px-5 h-100 text-center'>
+                    <img src={logo} className="mx-auto mb-4 w-50 img-fluid" alt="logo icon" />
                     <h5 className='mb-3'>Welcome to School Shopper</h5>
                     <h6 className='mb-1'>LOGIN TO YOUR ACCOUNT</h6>
                     <p>Enter your registered email and password to login to your account</p>
-                    <form action="" onSubmit={handleSubmit}>
-                        <input type="text" onClick={(e) => setEmail(e.target.value)} name="email" className='form-control mb-3' placeholder='Enter Username' />
-                        <input type="password" onClick={(e) => setPassword(e.target.value)} name="password" className='form-control mb-1' placeholder='Enter Username' />
-                        <div className='d-flex align-item-center justify-content-between'>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" onChange={(e) => setEmail(e.target.value)} name="email" className='form-control mb-3' placeholder='Enter Username' />
+                        <input type="password" onChange={(e) => setPassword(e.target.value)} name="password" className='form-control mb-1' placeholder='Enter Username' />
+                        <div className='d-flex justify-content-between align-item-center'>
                             <div>
                                 <input type="checkbox" className='me-2' />
                                 <label htmlFor="">Remember Me</label>
@@ -57,7 +62,7 @@ function Login() {
                                 <a href="#" className='text-primary text-decoration-none'>Forget Password?</a>
                             </div>
                         </div>
-                        <Button className='btn btn-dark w-100 mt-3'>Login to Continue </Button>
+                        <Button type="submit" className='mt-3 w-100 btn btn-dark'>Login to Continue </Button>
                     </form>
                 </div>
             </Col>
--- server/app.js
+++ server/app.js
@@ -16,12 +16,11 @@ connectDb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(cors());
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
 }));
 
 app.use('/public/uploads/', express.static(path.join(__dirname, '/public/uploads/')));
