c607ffa06c84202b2a6c57757c0a78fcdf04120e
Rishi <rishi.edunext@gmail.com>
2025-02-08 15:24:20 +0530

    feat: add Header and Sidebar components, integrate them into ProtectedRoute, and update styles and dependencies

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Form } from 'react-bootstrap';
+import { HiMagnifyingGlass } from "react-icons/hi2";
+import { useAuth } from '../context/AuthContext';
+
+function Header() {
+    const auth = useAuth();
+    console.log(auth.user);
+    
+    return (
+        <>
+            <div className="flex justify-between shadow-xl px-2 py-2 align-center">
+                <div className="relative search_bar">
+                    <HiMagnifyingGlass className='top-[12px] left-[10px] absolute' />
+                    <Form.Control className='ps-5' placeholder='Search here...' />
+                </div>
+                <div className="nav">
+                    <ul className='flex'>
+                        <li><a href="#">Home</a></li>
+                        <li>
+                            <div className='flex flex-col'>
+                                <p className='mb-0 font-[500] text-[14px]'>{auth.user?.name}</p>
+                                <p className='mb-0 text-[12px]'>{auth.user?.name}</p>
+                            </div>
+                        </li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
--- client/src/context/ProtectedRoute.jsx
+++ client/src/context/ProtectedRoute.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./AuthContext";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const ProtectedRoute = () => {
     const user = useAuth();
     if (!user.token) return <Navigate to="/" />;
-    return <Outlet />;
+    return (
+        <>
+            <Sidebar />
+            <Header />
+            <Outlet />
+        </>
+    );
 };
 
 export default ProtectedRoute;
