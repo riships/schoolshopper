76cba44e3926a6b9cfe18b545082fcaf26efea46
riships <rishi.edunext@gmail.com>
2025-05-17 16:33:11 +0530

    feat: update CustomDropdown component to accept placeholder prop and style dropdown items

--- client/src/components/CustomDropdown.jsx
+++ client/src/components/CustomDropdown.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Dropdown } from 'react-bootstrap';
 
 
-function CustomDropdown() {
+function CustomDropdown({
+    placeholder
+}) {
     return (
         <Dropdown >
-            <Dropdown.Toggle variant="action" id="dropdown-basic"></Dropdown.Toggle>
+            <Dropdown.Toggle variant="action" id="dropdown-basic">{placeholder}</Dropdown.Toggle>
             <Dropdown.Menu>
                 <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
                 <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
